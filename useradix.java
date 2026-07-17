0404fff7a39239b7f25c57a2286becfba1b1dcf9
Snehalkhot1 <khot6834@gmail.com>
2026-07-17 23:21:01 +0530

    Add files via upload

---
+++ useradix.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+class useradix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useRadix(2);
+        int x = sc.nextInt();
+        System.out.println(x);
+
+    }
+}
---
+++ valueUp.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class valueUp {
+    public static void main(String[] args) {
+        byte b =5;
+        System.out.println(b);
+        b=25;
+        System.out.println(b);
+    }
+}
