cba5fa55f6b113d6d079d409a34ec96a1abc1a4c
Snehalkhot1 <khot6834@gmail.com>
2026-07-19 21:29:43 +0530

    Add files via upload

---
+++ Binarybit.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+class Binarybit {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Binary representation of " + x + "is :" + Integer.toBinaryString(x));
+    }
+}
---
+++ Literal2.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+class Literal2 {
+    public static void main(String[] args) {
+        long l = 125;
+        System.out.println(l);
+    }
+}
