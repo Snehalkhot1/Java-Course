e7ea1e043f699b55ca719f3c93ca29fd3f1c6f2f
Snehalkhot1 <khot6834@gmail.com>
2026-07-20 23:05:00 +0530

    Add files via upload

---
+++ Arithemetic30.java
@@ -0,0 +1,25 @@
+//To proform the arthemetic operation on the varibles
+
+import java.lang.*;
+class Arithemetic30
+{
+    public static void main(String[] args) {
+        int a =14, b=5;
+
+        int c = a+b;
+        int d = a/b;
+        int e = a-b;
+        int m = a%b;
+        int f = a*b;
+
+        System.out.println("The Addition of the numbers is :" +c);
+        System.out.println("The Division of the numbers is :" + d);
+        System.out.println("The Substraction of the Numbers is :" + e);
+        System.out.println("The modulo of the numbers is :" + m);
+        System.out.println("The Multiplication of the numbers is :" + f);
+
+
+
+
+    }
+}
---
+++ Coricion.java
@@ -0,0 +1,23 @@
+//Write the program of the corision 
+
+//corision :: it means that it converts the data type of the particular variable that it is called it changes the data type internally
+
+import java.lang.*;
+class Coricion {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b =5;
+        float f = 2.5f; // to write the floatig value then write 'f' in front of it.
+        long x = 1234l; // to write the long value the write the 'l' in front of it.       
+        int c =a+b;
+
+        float h = f*x;
+        System.out.println("The addition is :" + c);
+        System.out.println("The multiplication is :" +h);
+
+
+        
+
+
+    }
+}
